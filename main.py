fcacbf77c2e66ebde1eb5b4fa880c5e38feda982
heheHwang <hehehwang@gmail.com>
2021-09-08 17:36:09 +0900

    fix

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ QUERY = config['General']['query']
 """
 TABLE data
 id : INTEGER
-name: TEXT
+file_name: TEXT
+file_path: TEXT
 sha : TEXT
 url : TEXT
 code : TEXT(BASE 64)
@@ -62,7 +63,8 @@ def checkDB():
     curr.execute("SELECT name FROM sqlite_master WHERE type='table'")
     lst = curr.fetchall()
     if len(lst) < 1:
-        curr.execute("CREATE TABLE data(id integer primary key autoincrement, name TEXT, sha TEXT unique , url TEXT, code TEXT, extension TEXT, Q TEXT)")
+        curr.execute("CREATE TABLE data(id integer primary key autoincrement, \
+        file_name TEXT, file_path TEXT, sha TEXT unique , url TEXT, code TEXT, extension TEXT, Q TEXT)")
     conn.close()
 
 def isDuplicated(value: str) -> bool:
@@ -82,71 +84,78 @@ def pushItemsToDB(items:list):
     conn = sqlite3.connect(DATABASE)
     curr = conn.cursor()
     for item in items:
-        name = item['name']
+        file_name = item['name']
+        file_path = item['path']
         sha = item['sha']
         url = item['url']
         query = item['query']
-        ext = name.split('.')[-1]
+        ext = file_name.split('.')[-1]
         code = item['code']
-        curr.execute('INSERT OR IGNORE INTO data (name, sha, url, code, extension, Q) VALUES (?, ?, ?, ?, ?, ?)',
-                         (name, sha, url, code, ext, query))
+        curr.execute('INSERT OR IGNORE INTO data (file_name, file_path, sha, url, code, extension, Q) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                         (file_name, file_path, sha, url, code, ext, query))
     conn.commit()
     conn.close()
+
 
 def crawlPage(sizedQuery:str, pageNo:int) -> bool:
     page = getSearchPageByCode(sizedQuery, pageNo)
     logger(f"CRAWLING Page #{pageNo}")
     items = []
 
-    # try:
-    for item in page['items']:
-        if isDuplicated(item['sha']):
-            logger(f"Page #{pageNo}, {cStr(item['name'], 'g')} is {cStr('DUPLICATED', 'r')}")
-            continue
+    try:
+        for item in page['items']:
+            if isDuplicated(item['sha']):
+                logger(f"#{CRAWLED_SIZE+1}-{pageNo}, {cStr(item['name'], 'g')} is {cStr('DUPLICATED', 'r')}")
+                continue
 
-        item['query'] = sizedQuery
-        item['code'] = getCodeFromItem(item)
-        if not item['code']:
-            logger(f"Page #{pageNo}, {cStr(item['name'], 'g')} is {cStr('NOT A FILE', 'bg')}")
-            continue
+            try:
+                item['code'] = getCodeFromItem(item)
+            except Exception as e:
+                logger(f"{CRAWLED_SIZE+1}-{pageNo}, {cStr(item['name'], 'g')} has {cStr('FAILED', 'br')} due to {str(e)}")
+                continue
 
-        items.append(item)
-        logger(f"Page #{pageNo}, {cStr(item['name'], 'g')} is {cStr('CRAWLED', 'bb')}")
-    logger(f"== Page #{pageNo} DONE ==")
-    pushItemsToDB(items)
-    logger(f"{cStr('Saved to DB.', 'b')}")
-    return True
+            if not item['code']:
+                logger(f"#{CRAWLED_SIZE+1}-{pageNo}, {cStr(item['name'], 'g')} is {cStr('NOT A FILE', 'bg')}")
+                continue
 
-    # except Exception as e:
-    #     logger(f"{cStr('Failed', 'r')} to crawl Page #{pageNo}, due to {e}")
-    #     errLogger(page, e)
-    #     return False
+            item['query'] = sizedQuery
+            items.append(item)
+            logger(f"#{CRAWLED_SIZE+1}-{pageNo}, {cStr(item['name'], 'g')} is {cStr('CRAWLED', 'bb')}")
+        logger(f"== Page #{pageNo} DONE ==")
+        pushItemsToDB(items)
+        logger(f"{cStr('Saved to DB.', 'b')}")
+        return True
+
+    except Exception as e:
+        logger(f"!!!! {cStr('Failed', 'r')} to crawl Page #{pageNo}, due to {e} !!!!")
+        errLogger(page, e)
+        return False
 
 def searchQuery(sizeIdx):
     global CRAWLED_PAGE
     sizedQuery = QUERY+f" size:{sizeIdx}..{sizeIdx+1}"
     page = getSearchPageByCode(sizedQuery)
-    # try:
-    results = min(1000, page['total_count'])
-    if not results:
-        logger(f"NO RESULTS IN {sizeIdx} to {sizeIdx+1}!!")
-    else:
-        logger(f"Found {cStr(page['total_count'], 'br')} codes, Crawling page: {CRAWLED_PAGE+1} to {results//100+1}")
-        while CRAWLED_PAGE < results//100+1:
-            pageToCrawl = CRAWLED_PAGE + 1
-            if crawlPage(sizedQuery, pageToCrawl):
-                CRAWLED_PAGE += 1
-                saveCheckpoint()
-            else:
-                logger("error, HALT")
-                return False
+    try:
+        results = min(1000, page['total_count'])
+        if not results:
+            logger(f"NO RESULTS IN {sizeIdx} to {sizeIdx+1}!!")
+        else:
+            logger(f"Found {cStr(page['total_count'], 'br')} codes, Crawling page: {CRAWLED_PAGE+1} to {results//100+1}")
+            while CRAWLED_PAGE < results//100+1:
+                pageToCrawl = CRAWLED_PAGE + 1
+                if crawlPage(sizedQuery, pageToCrawl):
+                    CRAWLED_PAGE += 1
+                    saveCheckpoint()
+                else:
+                    logger("error occurred, but we keep going anyway")
+                    return False
 
-    return True
+        return True
 
-    # except Exception as e:
-    #     logger(f"Failed to crawl Size #{sizeIdx}, due to {e}")
-    #     errLogger(page, e)
-    #     return False
+    except Exception as e:
+        logger(f"!!!! {cStr('Failed', 'r')} to crawl Size #{sizeIdx}, due to {e} !!!!")
+        errLogger(page, e)
+        return False
 
 def doCrawlBySize():
     global CRAWLED_SIZE, CRAWLED_PAGE
@@ -163,8 +172,10 @@ def doCrawlBySize():
             CRAWLED_SIZE += 1
             saveCheckpoint()
         else:
-            logger("error, HALT")
-            break
+            logger("error occurred, but we keep going anyway")
+
+
+
 
 if __name__ == '__main__':
     # {'incomplete_results': False, 'items': [], 'total_count': 0}
